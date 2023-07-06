99956c8661327302e7d04b1ad2dea7aa67e0dbeb
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2023-07-07 01:04:37 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css'
+import Board from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Board/>
+  </React.StrictMode>,
+)
+
